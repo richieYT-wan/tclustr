9db9acd039482ed014c7fd7404f32a8bd213dfbf
richieYT-wan <riwa@dtu.dk>
2023-09-28 18:18:13 +0200

    mod tcr sim kernel filepath

--- bashscripts/tcr_sim_kernel.sh
+++ bashscripts/tcr_sim_kernel.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/bash
 pepfile=${1}
+basename=$(basename ${pepfile})
 walltime=${2}
 script_content=$(cat <<EOF
-/home/projects/vaccine/people/morni/bin/pep2score_db_kernel -pa -t 2 ${pepfile} ${pepfile}> "/home/projects/vaccine/people/yatwan/tclustr/output/${1}_all_sim.out"
+/home/projects/vaccine/people/morni/bin/pep2score_db_kernel -pa -t 2 ${pepfile} ${pepfile} > "/home/projects/vaccine/people/yatwan/tclustr/output/${basename}_all_sim.out"
 EOF
 )
 echo "$script_content" > "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/TMP_tcr_sim.sh"
