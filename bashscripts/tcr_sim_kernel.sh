ed2c7ae290bd371e38b1fa91c94bc7516e800606
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-09-28 15:32:57 +0200

    mod tcr sim kernel

--- bashscripts/tcr_sim_kernel.sh
+++ bashscripts/tcr_sim_kernel.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/bash
-
 pepfile=${1}
-pepdb=${2}
-/home/projects/vaccine/people/morni/bin/pep2score_db_kernel -pa -t 2 ${pepfile} ${pepdb}
+walltime=${2}
+script_content=$(cat <<EOF
+/home/projects/vaccine/people/morni/bin/pep2score_db_kernel -pa -t 2 ${pepfile} ${pepfile}> "/home/projects/vaccine/people/yatwan/tclustr/output/${1}_all_sim.out"
+EOF
+)
+echo "$script_content" > "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/TMP_tcr_sim.sh"
+chmod +x "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/TMP_tcr_sim.sh"
+qsub -W group_list=vaccine -A vaccine -m e -l nodes=1:ppn=5:thinnode,mem=36gb,walltime=${2} "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/TMP_tcr_sim.sh"
+rm "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/TMP_tcr_sim.sh"
