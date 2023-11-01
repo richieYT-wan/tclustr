6b71631be13e1493cd48813465a3fb8fae8648ec
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-11-01 16:47:27 +0100

    pathing

--- bashscripts/231101_Extraction_CDRAB3.sh
+++ bashscripts/231101_Extraction_CDRAB3.sh
@@ -4,9 +4,11 @@ source activate pynn
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
 PYDIR=${HOMEDIR}pyscripts/
+DDIR=${HOMEDIR}output/2310XX_MoreChains/
+
 cd ${PYDIR}
 
-for f in 'CDR3_AB_nh_128_wd_1e-3_weights_3to1_i1RsR' 'CDR3_AB_nh_128_wd_1e-4_weights_3to1_LlWGn'
+for f in 'CDR3_AB_nh_128_wd_1e-3_weights_3to1_i1RsR/' 'CDR3_AB_nh_128_wd_1e-4_weights_3to1_LlWGn/'
 do
-  python3 redo_extract_agg_allfolds_CDR3AB.py -d ${f} -nh 128 -nl 64
+  python3 redo_extract_agg_allfolds_CDR3AB.py -d "${DDIR}${f}" -nh 128 -nl 64
 done
--- bashscripts/231101_Extraction_FullTCR.sh
+++ bashscripts/231101_Extraction_FullTCR.sh
@@ -4,11 +4,12 @@ source activate pynn
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
 PYDIR=${HOMEDIR}pyscripts/
+DDIR=${HOMEDIR}output/2310XX_MoreChains/
 cd ${PYDIR}
 
-for f in 'FullTCR_nh_128_wd_1e-3_weights_3to1_8NoCI' 'FullTCR_nh_128_wd_1e-4_weights_3to1_YTsp8'
+for f in 'FullTCR_nh_128_wd_1e-3_weights_3to1_8NoCI/' 'FullTCR_nh_128_wd_1e-4_weights_3to1_YTsp8/'
 do
-  python3 redo_extract_agg_allfolds_fulltcr.py -d ${f} -nh 128 -nl 64
+  python3 redo_extract_agg_allfolds_fulltcr.py -d "${DDIR}${f}" -nh 128 -nl 64
 done
 
-python3 redo_extract_agg_allfolds_fulltcr -d FullTCR_nh_256_wd_1e-4_weights_3to1_0UeYG -nh 256 -nl 128
+python3 redo_extract_agg_allfolds_fulltcr.py -d "${DDIR}FullTCR_nh_256_wd_1e-4_weights_3to1_0UeYG/" -nh 256 -nl 128
