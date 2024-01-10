9918f3d770ea29b79ef73a27ea08a6eda4246e46
richieYT-wan <riwa@dtu.dk>
2024-01-10 15:16:10 +0100

    mod scripts

--- bashscripts/240110_FullTCR_Bimodal_WarmUp_PepWeighted.sh
+++ bashscripts/240110_FullTCR_Bimodal_WarmUp_PepWeighted.sh
@@ -22,19 +22,19 @@ do
  filename="${outname}_fold_${f}_${random_string}"
   script_content=$(cat <<EOF
 source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
-source activate cuda
+source activate pynn
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
 PYDIR=\${HOMEDIR}pyscripts/
 filename=${filename}
 cd \${PYDIR}
-python3 ./231208_bimodal_vae.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/231205_nettcr_old_26pep_with_swaps.csv -pad -20 -enc BL50LO -cuda True -ne 5000 -lwseq 3 -lwkld 1e-2 -lwvae 1 -lwtrp .8 -lwclf 1 -dist_type cosine -margin 0.125 -mla1 7 -mla2 8 -mlb1 6 -mlb2 7 -nl 64 -nh 128 -nhclf 64 -do 0.2 -bn True -n_layers 1 -bs 1024 -lr 5e-4 -wd 1e-4 -wu 15 -wuclf 750 -pepenc BL50LO -o ${outname} -rid ${random_string} -kf ${f} -seed ${f} -pepweight True
+python3 ./231208_bimodal_vae.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/231205_nettcr_old_26pep_with_swaps.csv -pad -20 -enc BL50LO -cuda False -ne 5000 -lwseq 3 -lwkld 1e-2 -lwvae 1 -lwtrp .8 -lwclf 1 -dist_type cosine -margin 0.125 -mla1 7 -mla2 8 -mlb1 6 -mlb2 7 -nl 64 -nh 128 -nhclf 64 -do 0.2 -bn True -n_layers 1 -bs 1024 -lr 5e-4 -wd 1e-4 -wu 15 -wuclf 750 -pepenc BL50LO -o ${outname} -rid ${random_string} -kf ${f} -seed ${f} -pepweight True
 EOF
 )
                               # Write the script content to a file
                               echo "$script_content" > "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
                               chmod +x "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
-                              qsub -W group_list=vaccine -A vaccine -m e -l nodes=1:ppn=40,mem=36gb,walltime=${1} "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
+                              qsub -W group_list=vaccine -A vaccine -m e -l nodes=1:ppn=8:thinnode,mem=36gb,walltime=${1} "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
                               rm "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
 
 done
