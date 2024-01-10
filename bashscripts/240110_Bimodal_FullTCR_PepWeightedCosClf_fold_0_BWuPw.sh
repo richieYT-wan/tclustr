4f677315860eab46899d3b7d8f640f4d9a16620b
richieYT-wan <riwa@dtu.dk>
2024-01-10 15:24:16 +0100

    added each fold script for pepweighted

---
+++ bashscripts/240110_Bimodal_FullTCR_PepWeightedCosClf_fold_0_BWuPw.sh
@@ -0,0 +1,8 @@
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate pynn
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
+PYDIR=${HOMEDIR}pyscripts/
+# filename=240110_Bimodal_FullTCR_PepWeightedCosClf_fold_0_BWuPw
+cd ${PYDIR}
+python3 ./231208_bimodal_vae.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/231205_nettcr_old_26pep_with_swaps.csv -pad -20 -enc BL50LO -cuda False -ne 5000 -lwseq 3 -lwkld 1e-2 -lwvae 1 -lwtrp .8 -lwclf 1 -dist_type cosine -margin 0.125 -mla1 7 -mla2 8 -mlb1 6 -mlb2 7 -nl 64 -nh 128 -nhclf 64 -do 0.2 -bn True -n_layers 1 -bs 1024 -lr 5e-4 -wd 1e-4 -wu 15 -wuclf 750 -pepenc BL50LO -o 240110_Bimodal_FullTCR_PepWeightedCosClf -rid BWuPw -kf 0 -seed 0 -pepweight True
---
+++ bashscripts/240110_FullTCR_PepWeightedCosTrp_fold_4_FCoPw.sh
@@ -0,0 +1,8 @@
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate pynn
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
+PYDIR=${HOMEDIR}pyscripts/
+filename=240110_FullTCR_PepWeightedCosTrp_fold_4_FCoPw
+cd ${PYDIR}
+python3 ./231102_fulltcr_tripletloss.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/230927_nettcr_positives_only.csv -pad -20 -enc BL50LO -ne 20000 -cuda False -lwseq 3 -lwkld 1e-2 -lwtrp 0.8 -dist_type cosine -margin 0.125 -mla1 7 -mla2 8 -mla3 22 -mlb1 6 -mlb2 7 -mlb3 23 -nl 64 -nh 128 -bs 512 -lr 5e-4 -wd 1e-4 -wu 15 -o 240110_FullTCR_PepWeightedCosTrp -rid FCoPw -kf 4 -seed 4 -pepweight True
