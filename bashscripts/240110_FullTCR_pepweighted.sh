df62fb3f36a4370a1596592cc24d13bafb97720c
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-01-10 15:17:09 +0100

    removed qsub for debugging

--- bashscripts/240110_FullTCR_pepweighted.sh
+++ bashscripts/240110_FullTCR_pepweighted.sh
@@ -28,14 +28,14 @@ HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
 PYDIR=\${HOMEDIR}pyscripts/
 filename=${filename}
 cd \${PYDIR}
-python3 ./231102_fulltcr_tripletloss.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/230927_nettcr_positives_only.csv -pad -20 -enc BL50LO -ne 25000 -cuda False -lwseq 3 -lwkld 1e-2 -lwtrp 0.8 -dist_type cosine -margin 0.125 -mla1 7 -mla2 8 -mla3 22 -mlb1 6 -mlb2 7 -mlb3 23 -nl 64 -nh 128 -bs 512 -lr 5e-4 -wd 1e-4 -wu 15 -o ${outname} -rid ${random_string} -kf ${f} -seed ${f} -pepweight True
+python3 ./231102_fulltcr_tripletloss.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/230927_nettcr_positives_only.csv -pad -20 -enc BL50LO -ne 20000 -cuda False -lwseq 3 -lwkld 1e-2 -lwtrp 0.8 -dist_type cosine -margin 0.125 -mla1 7 -mla2 8 -mla3 22 -mlb1 6 -mlb2 7 -mlb3 23 -nl 64 -nh 128 -bs 512 -lr 5e-4 -wd 1e-4 -wu 15 -o ${outname} -rid ${random_string} -kf ${f} -seed ${f} -pepweight True
 EOF
 )
                               # Write the script content to a file
                               echo "$script_content" > "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
                               chmod +x "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
-                              qsub -W group_list=vaccine -A vaccine -m e -l nodes=1:ppn=8:thinnode,mem=36gb,walltime=${1} "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
-                              rm "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
+                              # qsub -W group_list=vaccine -A vaccine -m e -l nodes=1:ppn=8:thinnode,mem=36gb,walltime=${1} "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
+                              # rm "/home/projects/vaccine/people/yatwan/tclustr/bashscripts/${filename}.sh"
 
 done
 
