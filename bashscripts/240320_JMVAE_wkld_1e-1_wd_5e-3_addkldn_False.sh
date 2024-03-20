126ea27d243050250059f09a4c3f3b6de16000fe
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-03-20 15:14:36 +0100

    added logs

--- bashscripts/240320_JMVAE_wkld_1e-1_wd_5e-3_addkldn_False.sh
+++ bashscripts/240320_JMVAE_wkld_1e-1_wd_5e-3_addkldn_False.sh
@@ -5,4 +5,4 @@ HOMEDIR=/home/projects2/riwa/tclustr/
 PYDIR=${HOMEDIR}pyscripts/
 filename=JMVAE_10kepochs_wkld_1e-1_wd_5e-3_addkldn_False
 cd ${PYDIR}
-python3 240320_JMVAE.py -f ../data/multimodal/240314_multimodal_NO_HUMAN_tcr_pep.csv -cuda True -pad -20 -enc BL50LO -addpe False -nhtcr 200 -nhpep 150 -nl 100 -act selu -do 0.25 -bn True -lr 1e-4 -bs 1024 -tol 1e-3 -lwseq 1 -lwkld_z 1  -debug False -pepweight False -kf 0 -seed 0 -o ${filename} -pair_only True -return_pair True -ne 10000 -wukld 800 -kldts 0.01 -fp 100 -kld_dec 5e-4 -wd 5e-3 -addkldn False -lwkld_n 1e-1 -device cuda:1
+python3 240320_JMVAE.py -f ../data/multimodal/240314_multimodal_NO_HUMAN_tcr_pep.csv -cuda True -pad -20 -enc BL50LO -addpe False -nhtcr 200 -nhpep 150 -nl 100 -act selu -do 0.25 -bn True -lr 1e-4 -bs 1024 -tol 1e-3 -lwseq 1 -lwkld_z 1  -debug False -pepweight False -kf 0 -seed 0 -o ${filename} -pair_only True -return_pair True -ne 10000 -wukld 800 -kldts 0.01 -fp 100 -kld_dec 5e-4 -wd 5e-3 -addkldn False -lwkld_n 1e-1 -device cuda:1 > "${HOMEDIR}logs/$(date '+%y%m%d_%H%M')_${filename}.log" 2>&1
