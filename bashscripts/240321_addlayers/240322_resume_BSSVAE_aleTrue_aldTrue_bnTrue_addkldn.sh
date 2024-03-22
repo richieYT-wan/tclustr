75f13f8c4e739794982584b38ce497c23aee0c45
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-03-22 14:12:31 +0100

    uptd

--- bashscripts/240321_addlayers/240322_resume_BSSVAE_aleTrue_aldTrue_bnTrue_addkldn.sh
+++ bashscripts/240321_addlayers/240322_resume_BSSVAE_aleTrue_aldTrue_bnTrue_addkldn.sh
@@ -7,6 +7,6 @@ PYDIR=${HOMEDIR}pyscripts/
 add_layer_enc=True
 add_layer_dec=True
 batchnorm=True
-filename="RESUME_ADDKLDN_BSSVAE_addlenc${add_layer_enc}_addldec${add_layer_dec}_bn${batchnorm}_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100"
+filename="RESUME_ADDKLDN_BSSVAE_allTrue_10kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100"
 cd ${PYDIR}
-python3 240321_resume_multimodal.py -model_folder ../output/240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy/ -o ${filename} -wd 1e-8 -ne 10000 -cuda True -device cuda:0 > "${HOMEDIR}logs/$(date '+%y%m%d_%H%M')_${filename}.log" 2>&1
+python3 240321_resume_multimodal.py -model_folder ../output/240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy/ -o ${filename} -wd 1e-8 -ne 10000 -cuda True -device cuda:0 -addkldn True > "${HOMEDIR}logs/$(date '+%y%m%d_%H%M')_${filename}.log" 2>&1
