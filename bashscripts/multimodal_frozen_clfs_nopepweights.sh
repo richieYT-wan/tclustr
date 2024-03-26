87f1c300226c15b5af6c93aee164f1a56a000794
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-03-26 13:14:26 +0100

    Update multimodal_frozen_clfs_nopepweights.sh

--- bashscripts/multimodal_frozen_clfs_nopepweights.sh
+++ bashscripts/multimodal_frozen_clfs_nopepweights.sh
@@ -47,9 +47,9 @@ for f in ${subdir}; do
     echo ${best_checkpoint}
     echo ${last_checkpoint}
     # Doing with "best" checkpoint
-    python3 ./train_classifier_frozen_mmvae.py -json_file ${json_file} -pt_file ${best_checkpoint} -rid ${random_id} -od "${2}/BEST_${folder_name}/" -o "BEST_${name_description}" -cuda True -f ../data/multimodal/240326_nettcr_paired_withswaps.csv -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 1e-5 -bs 1024 -ne ${3} -pepenc 'none' -pepweight False -kf ${kfold_number} -seed ${kfold_number}
+    python3 ./train_classifier_frozen_mmvae.py -json_file ${json_file} -pt_file ${best_checkpoint} -rid ${random_id} -od "${2}/BEST_${folder_name}/" -o "BEST_${name_description}" -cuda True -f ../data/multimodal/240326_nettcr_paired_withswaps.csv -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 1e-5 -bs 2048 -ne ${3} -pepenc 'none' -pepweight False -kf ${kfold_number} -seed ${kfold_number}
     # Doing with "last" checkpoint
-    python3 ./train_classifier_frozen_mmvae.py -json_file ${json_file} -pt_file ${last_checkpoint} -rid ${random_id} -od "${2}/LAST_${folder_name}/" -o "LAST_${name_description}" -cuda True -f ../data/multimodal/240326_nettcr_paired_withswaps.csv -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 1e-5 -bs 1024 -ne ${3} -pepenc 'none' -pepweight False -kf ${kfold_number} -seed ${kfold_number}
+    python3 ./train_classifier_frozen_mmvae.py -json_file ${json_file} -pt_file ${last_checkpoint} -rid ${random_id} -od "${2}/LAST_${folder_name}/" -o "LAST_${name_description}" -cuda True -f ../data/multimodal/240326_nettcr_paired_withswaps.csv -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 1e-5 -bs 2048 -ne ${3} -pepenc 'none' -pepweight False -kf ${kfold_number} -seed ${kfold_number}
     echo "------------------------------------------------------------------------"
     echo ""
 done
