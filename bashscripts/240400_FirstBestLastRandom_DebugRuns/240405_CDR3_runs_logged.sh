706416ba59096426ad21830a19e9463616b0fec3
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-04-05 12:05:57 +0200

    added runs and logs

--- bashscripts/240400_FirstBestLastRandom_DebugRuns/240405_CDR3_runs_logged.sh
+++ bashscripts/240400_FirstBestLastRandom_DebugRuns/240405_CDR3_runs_logged.sh
@@ -2,47 +2,19 @@
 # Do a new model run using both a Normal VAE and a BSSVAE
 # compare with run with normal VAE with all CDRs as well (see 240404_all_runs_logged last command)
 
-# Do 2 "blank" runs of CDR3 VAEs and BSSVAEs (See 240405_CDR3_only_VAEs and save the first epoch only so I can already pre-run the analysis)
+# Do 2 "blank" runs of CDR3 VAEs and BSSVAEs loading the first epoch (See 240405_CDR3_only_VAEs and save the first epoch only so I can already pre-run the analysis)
+# Do 2 blank runs of CDR3 VAEs and BSSVAEs loading then resetting the weights
+HDIR="/Users/riwa/Documents/code/tclustr/"
+MDIR="${HDIR}output/240405_CDR3_VAEs/"
+df="/Users/riwa/Documents/code/tclustr/data/multimodal/240326_nettcr_paired_withswaps.csv"
+outdir="${HDIR}output/240404_RESET_PARAMS_LATENT_TEST/"
+# CDR3 old VAE runs : 1 blank (load checkpoint_1)
+#                     1 reset (load + reset
+cd "${HDIR}pyscripts/"
+nohup python3 train_classifier_frozen_vae.py -json_file "${MDIR}/CDR3_1stage_SMALL_SmCDR3/checkpoint_json.json" -pt_file "${MDIR}/CDR3_1stage_SMALL_SmCDR3/checkpoint_1.pt" -rid SmCDR3 -od ${outdir} -o CDR3_1stage_NO_RESET -cuda False -f ${df} -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500 -pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset False > ${HDIR}/logs/240405_CDR3_1st_Epoch_1.log 2>&1
 
+nohup python3 train_classifier_frozen_vae.py -json_file "${MDIR}/CDR3_1stage_SMALL_SmCDR3/checkpoint_json.json" -pt_file "${MDIR}/CDR3_1stage_SMALL_SmCDR3/checkpoint_1.pt" -rid SmCDR3 -od ${outdir} -o CDR3_1stage_WITH_RESET -cuda False -f ${df} -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500 -pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset True > ${HDIR}/logs/240405_CDR3_1st_RESET.log 2>&1
 
-nohup python3 train_classifier_frozen_mmvae.py -json_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/checkpoint_best_kcv_f00_240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH_JSON_kwargs.json
--pt_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/epoch_1_interval_checkpoint__fold00_kcv_240314_multimodal_NO_HUMAN_tcr_pep_f00_240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy.pt
--rid IzcVOH -od RESET_PARAMS_LATENT_TEST/ -o NO_RESET -cuda False
--f /Users/riwa/Documents/code/tclustr/data/multimodal/240326_nettcr_paired_withswaps.csv
--nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500
--pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset False > ../logs/240404_TestFrozenMMVAECLF_NoReset.log 2>&1 ;
+nohup python3 train_classifier_frozen_mmvae.py -json_file "${MDIR}/CDR3_BSSVAE_frVvuv/checkpoint_json.json" -pt_file "${MDIR}/CDR3_BSSVAE_frVvuv/checkpoint_1.pt" -rid frVvuv -od ${outdir} -o CDR3_BSSVAE_NO_RESET -cuda False -f ${df} -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500 -pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset False > ${HDIR}/logs/240405_CDR3_BSS_Epoch_1.log 2>&1
 
-
-nohup python3 train_classifier_frozen_mmvae.py -json_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/checkpoint_best_kcv_f00_240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH_JSON_kwargs.json
--pt_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/epoch_1_interval_checkpoint__fold00_kcv_240314_multimodal_NO_HUMAN_tcr_pep_f00_240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy.pt
--rid IzcVOH -od RESET_PARAMS_LATENT_TEST/ -o RESET_PARAMS -cuda False
--f /Users/riwa/Documents/code/tclustr/data/multimodal/240326_nettcr_paired_withswaps.csv
--nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500
--pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset True > ../logs/240404_TestFrozenMMVAECLF_RESETONLY.log 2>&1
-
-
-nohup python3 train_classifier_frozen_mmvae.py -json_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/checkpoint_best_kcv_f00_240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH_JSON_kwargs.json
--pt_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/epoch_1_interval_checkpoint__fold00_kcv_240314_multimodal_NO_HUMAN_tcr_pep_f00_240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy.pt
--rid IzcVOH -od RESET_PARAMS_LATENT_TEST/ -o RANDOM_LATENT -cuda False
--f /Users/riwa/Documents/code/tclustr/data/multimodal/240326_nettcr_paired_withswaps.csv
--nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500
--pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset False -random_latent True > ../logs/240404_TestFrozenMMVAECLF_RANDOMLATENT.log 2>&1
-
-# Run using
-nohup python3 train_classifier_frozen_mmvae.py -json_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/checkpoint_best_kcv_f00_240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH_JSON_kwargs.json
--pt_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/epoch_1_interval_checkpoint__fold00_kcv_240314_multimodal_NO_HUMAN_tcr_pep_f00_240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy.pt
--rid IzcVOH -od RESET_PARAMS_LATENT_TEST/ -o RESET_NOPEPENC -cuda False
--f /Users/riwa/Documents/code/tclustr/data/multimodal/240326_nettcr_paired_withswaps.csv
--nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500
--pepenc none -pepweight False -kf 0 -seed 0 -reset True -random_latent False > ../logs/240404_TestFrozenMMVAECLF_RESET_NOPEPENC.log 2>&1 &
-
-# Run using df without "original_peptide" column and reset model
-nohup python3 train_classifier_frozen_mmvae.py -json_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/checkpoint_best_kcv_f00_240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH_JSON_kwargs.json
--pt_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/epoch_1_interval_checkpoint__fold00_kcv_240314_multimodal_NO_HUMAN_tcr_pep_f00_240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy.pt
--rid IzcVOH -od RESET_PARAMS_LATENT_TEST/ -o RESET_NOoriginal -cuda False
--f /Users/riwa/Documents/code/tclustr/data/multimodal/240404_nettcr_swapped_no_original.csv
--nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500 -pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset True -random_latent False > ../logs/240404_TestFrozenMMVAECLF_RESET_NOoriginal.log 2>&1 &
-
-# RUN Using TCR Blosum only
-nohup python3 train_classifier_frozen_mmvae.py -json_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/checkpoint_best_kcv_f00_240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH_JSON_kwargs.json -pt_file /Users/riwa/Documents/code/tclustr/output/240404_FirstBestLast_comparison/mmvaes/240325_1327_BSSVAE_addlencTrue_addldecTrue_bnTrue_LONG25kepochs_wd1e-6_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_IzcVOH/epoch_1_interval_checkpoint__fold00_kcv_240314_multimodal_NO_HUMAN_tcr_pep_f00_240321_1427_BSSVAE_addlencTrue_addldecTrue_bnTrue_2p5kepochs_wd5e-5_lwseq2_nhtcr256_nhpep128_nl100_KFold_0_VoZuWy.pt -rid IzcVOH -od RESET_PARAMS_LATENT_TEST/ -o NEW_MODEL_TCRBLSM -cuda False -f /Users/riwa/Documents/code/tclustr/data/multimodal/240326_nettcr_paired_withswaps.csv -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500 -pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset True -newmodel True -tcr_enc BL50LO
-
+nohup python3 train_classifier_frozen_mmvae.py -json_file "${MDIR}/CDR3_BSSVAE_frVvuv/checkpoint_json.json" -pt_file "${MDIR}/CDR3_BSSVAE_frVvuv/checkpoint_1.pt" -rid frVvuv -od ${outdir} -o CDR3_BSSVAE_WITH_RESET -cuda False -f ${df} -nh 64 -do 0.25 -bn True -n_layers 1 -lr 1e-4 -wd 5e-6 -bs 2048 -ne 1500 -pepenc BL50LO -pepweight False -kf 0 -seed 0 -reset True > ${HDIR}/logs/240405_CDR3_BSS_RESET.log 2>&1
