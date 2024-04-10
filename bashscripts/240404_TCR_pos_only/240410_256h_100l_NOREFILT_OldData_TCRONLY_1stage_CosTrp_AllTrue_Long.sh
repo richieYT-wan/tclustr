933d2886a12d655cccc797fc57bc939222bba8eb
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-04-10 22:49:54 +0200

    fixed paths

---
+++ bashscripts/240404_TCR_pos_only/240410_256h_100l_NOREFILT_OldData_TCRONLY_1stage_CosTrp_AllTrue_Long.sh
@@ -0,0 +1,7 @@
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate cuda
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
+PYDIR=${HOMEDIR}pyscripts/
+cd ${PYDIR}
+python3 ./231102_fulltcr_tripletloss.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/filtered/231205_nettcr_old_26pep_with_swaps.csv -pad -20 -enc BL50LO -ne 12000 -cuda True -lwseq 1 -lwkld 1e-2 -lwtrp 3 -dist_type cosine -margin 0.075 -mla1 7 -mla2 8 -mla3 22 -mlb1 6 -mlb2 7 -mlb3 23 -mlpep 0 -nl 100 -nh 256 -bs 1024 -lr 5e-4 -wd 1e-5 -wu 100 -o OLDDATA_TCRONLY_1stage_LARGE_256h_100l_CosTrp -rid OLDCosTCRP1 -kf 0 -seed 0 -addpe True -bn True -ale True -ald True -ob False -pepweight True -posweight True > "${HOMEDIR}logs/OLDDATA_TCRONLY_1stage_LARGE_256h_100l_CosTrp.log" 2>&1
