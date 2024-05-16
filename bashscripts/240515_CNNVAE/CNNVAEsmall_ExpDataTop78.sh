ff841c0d07deb50f614bcddf37013d843977b765
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-05-16 09:55:21 +0200

    puta madre

--- bashscripts/240515_CNNVAE/CNNVAEsmall_ExpDataTop78.sh
+++ bashscripts/240515_CNNVAE/CNNVAEsmall_ExpDataTop78.sh
@@ -1,6 +1,6 @@
-HOMEDIR=/Users/riwa/Documents/code/tclustr/
+HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
 PYDIR=${HOMEDIR}pyscripts/
-source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
 source activate cuda
 
 # Get the full path of the script
@@ -18,4 +18,4 @@ cd ${PYDIR}
 # sed ExpModel17_ExpDataTop78 with Outname == ModelName_InputDFname
 # sed ExpDataTop78 with ExpDataTop78 == InputDFname
 # sed 240426_NewMarginFullExp17Peps/240426_1604_17Peps_KL_1e-2_0200_KFold_0_AllTriplet with actual path
-python3 ./240420_VAE_Clustering_intervals.py -np 500 -kf 0 -o CNNVAEModelFull_ExpDataTop78 -od ../output/240515_IntervalClustering/ExpDataTop78/ -tbcralign ../output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv -tcrdist ../output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv -f /Users/riwa/Documents/code/tclustr/data/filtered/240507_nettcr_exp_pruned_noswap_78peps.csv -model_folder ../output/240515_1205_CNNVAE_KF0_GroupSamplerSmaller_KFold_0_wgfoUx -rb True -bf ../output/240515_IntervalClustering -n_jobs 8 -dn ExpDataTop78
+python3 ./240420_VAE_Clustering_intervals.py -np 500 -kf 0 -o CNNVAEModelFull_ExpDataTop78 -od ../output/240515_IntervalClustering/ExpDataTop78/ -tbcralign ../output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv -tcrdist ../output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv -f /Users/riwa/Documents/code/tclustr/data/filtered/240507_nettcr_exp_pruned_noswap_78peps.csv -model_folder ../output/240515_1205_CNNVAE_KF0_GroupSamplerSmaller_KFold_0_wgfoUx -rb True -bf ../output/240515_IntervalClustering -n_jobs 20 -dn ExpDataTop78
