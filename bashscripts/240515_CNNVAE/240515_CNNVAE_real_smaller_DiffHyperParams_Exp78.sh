c0b849e10625d427f8565c558bb39e8ff31abf32
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-05-16 16:58:09 +0200

    added diff cnnmodels

---
+++ bashscripts/240515_CNNVAE/240515_CNNVAE_real_smaller_DiffHyperParams_Exp78.sh
@@ -0,0 +1,15 @@
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate cuda
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
+PYDIR=${HOMEDIR}pyscripts/
+# Get the full path of the script
+script_path="$(readlink -f "$0")"
+
+# Extract the basename of the script
+script_name="$(basename "$script_path" .sh)"
+
+echo "The basename of the script is: $script_name"
+
+cd ${PYDIR}
+python3 ./240515_cnnvae_tripletloss.py -f /home/projects/vaccine/people/yatwan/tclustr/data/filtered/240507_nettcr_exp_pruned_noswap_78peps.csv -pad -20 -enc BL50LO -ne 30000 -cuda True -lwseq 1 -lwkld 1e-2 -lwtrp 10 -dist_type cosine -margin 0.2 -mla1 7 -mla2 8 -mla3 22 -mlb1 6 -mlb2 7 -mlb3 23 -mlpep 0 -nl 100 -nh 128 -bs 512 -lr 2.5e-4 -wd 1e-4 -wu 150 -fp 150 -kld_dec 1e-3 -kldts 0.075 -o "CNNVAE_78peps_KF0_GroupSamplerSmaller_KL1e-1_LWT10_30kepochs" -kf 0 -seed 0 -addpe True -bn True -pepweight False -posweight True -minority_sampler True -minority_count 50 > "${HOMEDIR}logs/240515_CNNVAE_78peps_Real_Small_KL1e-1_LWT10_30kepochs.log" 2>&1
