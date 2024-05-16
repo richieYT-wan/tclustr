deeffa408ff369fd2580560c22734ede8a53d786
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-05-16 13:31:56 +0200

    vntr

--- bashscripts/240510_TripletTweaks_Clustering_computerome/TripletTweak_Expanded_CDTrue_WUFalse_SamplerTrue_P0N1_Exp78Peps.sh
+++ bashscripts/240510_TripletTweaks_Clustering_computerome/TripletTweak_Expanded_CDTrue_WUFalse_SamplerTrue_P0N1_Exp78Peps.sh
@@ -9,4 +9,4 @@ tbcralign=/home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringT
 tcrdist=/home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv
 iid=Exp78Peps
 idf=/home/projects/vaccine/people/yatwan/tclustr/data/filtered/240507_nettcr_exp_pruned_noswap_78peps.csv
-python3 ./240420_VAE_Clustering_intervals.py -np 400 -kf 0 -o ${iid}_TripletTweak_Expanded_CDTrue_WUFalse_SamplerTrue_P0N1 -od ../output/240516_TripletTweaks_IntervalClustering/ -tbcralign ${tbcralign} -tcrdist ${tcrdist} -f ${idf} -n_jobs 40 -dn ExpDataTop78 -rb True -bf ../output/240515_IntervalClustering/ -model_folder ${model_folder}
+python3 ./240420_VAE_Clustering_intervals.py -np 400 -kf 0 -o ${iid}_TripletTweak_Expanded_CDTrue_WUFalse_SamplerTrue_P0N1 -od ../output/240516_TripletTweaks_IntervalClustering/${iid} -tbcralign ${tbcralign} -tcrdist ${tcrdist} -f ${idf} -n_jobs 40 -dn ExpDataTop78 -rb True -bf ../output/240515_IntervalClustering/ -model_folder ${model_folder}
