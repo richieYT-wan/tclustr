860e12dda62eb201d9bc22dd34e5a698490f4df5
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-05-21 10:25:28 +0200

    database mistake

--- bashscripts/240516_Clustering_CNNVAEs/make_scripts.sh
+++ bashscripts/240516_Clustering_CNNVAEs/make_scripts.sh
@@ -7,13 +7,13 @@ for model_size in "${ms[@]}";do
 		# Define the inputs and baselines
 		if [ "$ds" = "78peps" -o "$ds" = "ExpAll" ]; then
 			input_df=(/home/projects/vaccine/people/yatwan/tclustr/data/filtered/240418_nettcr_expanded_20binders_17pep_POSONLY.csv /home/projects/vaccine/people/yatwan/tclustr/data/filtered/240507_nettcr_exp_pruned_noswap_78peps.csv)
-			input_id=(Exp17Peps Exp78Peps)
+			input_id=(ExpData17peps ExpDataTop78)
 			tbcralign=/home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv
 			tcrdist=/home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv
 
 		elif [ "$ds" = "Old20peps" -o "ds" = "OldAll" ]; then
 			input_df=(/home/projects/vaccine/people/yatwan/tclustr/data/filtered/240507_nettcr_old_pruned_noswap_20peps.csv /home/projects/vaccine/people/yatwan/tclustr/data/filtered/240416_nettcr_old_26pep_no_swaps.csv /home/projects/vaccine/people/yatwan/tclustr/data/filtered/240416_nettcr_old_top15peps_no_swaps.csv)
-			input_id=(Old20peps OldFull Old15Peps)
+			input_id=(OldDataTop20 OldDataNoPrune OldDataTop15)
 			tbcralign=/home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_26peps_labeled.csv 
 			tcrdist=/home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_26peps_old_labeled.csv
 
