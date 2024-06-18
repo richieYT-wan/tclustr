384794013cd464dc6c658637d111033a506aff3e
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-06-18 16:00:57 +0200

    updated fold and output directory

--- bashscripts/240618_ReBenchmarkCNN_Nested/Nested_CNNVAE_latent_256_kld_1e-2_train_ExpAll.sh
+++ bashscripts/240618_ReBenchmarkCNN_Nested/Nested_CNNVAE_latent_256_kld_1e-2_train_ExpAll.sh
@@ -2,7 +2,7 @@ source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
 source activate cuda
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
-outdir="240527_DiagnosticRound2"
+outdir="240618_NestedKCV_CNNVAE"
 RESDIR="${HOMEDIR}output/${outdir}"
 PYDIR=${HOMEDIR}pyscripts/
 cd ${PYDIR}
@@ -26,9 +26,9 @@ outmatch=$(ls -t ${RESDIR} | grep ${random_id} | head -n 1)
 iid=ExpData17peps
 idf=/home/projects/vaccine/people/yatwan/tclustr/data/filtered/240418_nettcr_expanded_20binders_17pep_POSONLY.csv
 # Run clustering part
-python3 ./240618_nested_VAE_Clustering_intervals.py -rid ${random_id} -np 500 -kf 0 -o ${outname}_${iid} -od ../output/${outdir}/clustering/ -tbcralign /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv -tcrdist /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv -f ${idf} -model_folder "${RESDIR}/${outmatch}" -rb True -n_jobs 40 -dn ${iid} -bf ../output/240515_IntervalClustering
+python3 ./240618_nested_VAE_Clustering_intervals.py -rid ${random_id} -np 500 -kf 0 -if 1 -o ${outname}_${iid} -od ../output/${outdir}/clustering/ -tbcralign /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv -tcrdist /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv -f ${idf} -model_folder "${RESDIR}/${outmatch}" -rb True -n_jobs 40 -dn ${iid} -bf ../output/240515_IntervalClustering
 outmatch=$(ls -t ${RESDIR} | grep ${random_id} | head -n 1)
 iid=ExpDataTop78
 idf=/home/projects/vaccine/people/yatwan/tclustr/data/filtered/240507_nettcr_exp_pruned_noswap_78peps.csv
 # Run clustering part
-python3 ./240618_nested_VAE_Clustering_intervals.py -rid ${random_id} -np 500 -kf 0 -o ${outname}_${iid} -od ../output/${outdir}/clustering/ -tbcralign /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv -tcrdist /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv -f ${idf} -model_folder "${RESDIR}/${outmatch}" -rb True -n_jobs 40 -dn ${iid} -bf ../output/240515_IntervalClustering
+python3 ./240618_nested_VAE_Clustering_intervals.py -rid ${random_id} -np 500 -kf 0 -if 1 -o ${outname}_${iid} -od ../output/${outdir}/clustering/ -tbcralign /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/2404XX_OUTPUT_tbcralign_distmatrix_140peps_labeled.csv -tcrdist /home/projects/vaccine/people/yatwan/tclustr/output/240411_ClusteringTests/dist_matrices/tcrdist3_distmatrix_140peps_new_labeled.csv -f ${idf} -model_folder "${RESDIR}/${outmatch}" -rb True -n_jobs 40 -dn ${iid} -bf ../output/240515_IntervalClustering
