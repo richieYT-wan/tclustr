3f57718960b1906c68f6c86a22ec58b6fa5fad77
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-09-11 10:40:18 +0200

    Create make_logs.sh

---
+++ bashscripts/make_logs.sh
@@ -0,0 +1,16 @@
+for f in $(ls $1/*.sh.e*);  do
+  name="${f%.sh.*}";
+  job_id="${f##*.sh.e}";
+  file_content=$(cat "${f}");
+  if echo "$file_content" | grep -qi "error";
+  then
+    error_bool="True";
+    tail_file=$(tail -n 5 "${f}");
+    echo "${name}  ${job_id} ${error_bool}" >> $1/all_error_logs.txt;
+    echo "${tail_file}" >> $1/all_error_logs.txt;
+  else
+    error_bool="False";
+    echo "${name}  ${job_id} ${error_bool}" >> $1/all_error_logs.txt;
+  fi;
+  echo " " >> $1/all_error_logs.txt;
+done
