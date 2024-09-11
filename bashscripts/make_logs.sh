48abc7ea3855c1ca9d8233840da08e2969698b79
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-09-11 23:37:39 +0200

    actually remove the job_id might as well

--- bashscripts/make_logs.sh
+++ bashscripts/make_logs.sh
@@ -12,7 +12,7 @@ for f in $(ls $1/*.sh.e*);  do
   else
     error_bool="False";
     echo "No errors for ${name} ${job_id}. Removing log"
-#    rm $1/*${job_id}*
+    rm $1/*${job_id}*
     echo "${name} ${job_id} ${error_bool}" >> $1/all_error_logs.txt;
   fi;
   echo " " >> $1/all_error_logs.txt;
