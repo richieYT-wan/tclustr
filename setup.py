0390f75607f4a5f460aded39ddfba1d497f6044b
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-10-01 14:10:42 +0200

    Revert "Update setup.py"
    This reverts commit 255a52de75f0468f439532646eb9c9724cf65b9a.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='tclustr',
     version='1.0',
-    packages=find_packages()
+    packages=find_packages(where='src'),
+    pacckage_dir={'':'src'},
 )
