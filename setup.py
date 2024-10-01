255a52de75f0468f439532646eb9c9724cf65b9a
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-10-01 14:00:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='tclustr',
     version='1.0',
-    packages=find_packages(where='src'),
-    pacckage_dir={'':'src'},
+    packages=find_packages()
 )
