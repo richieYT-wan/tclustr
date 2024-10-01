c4ecc435abe8dcd4995007920fdf22718d488cdb
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-10-01 14:09:57 +0200

    Revert "updated fuill paths"
    This reverts commit 14d3608f8e3491da3adb68aea0eda115f9fdd5cf.

--- bashscripts/do_tbcralign.sh
+++ bashscripts/do_tbcralign.sh
@@ -10,7 +10,6 @@ EXTRACOLS=("original_peptide" "binder" "partition" "original_index")
 TBCRALIGN="$HTCPATH"
 chainarg="full"
 OUTPUTDIRECTORY=""
-HOMEDIR="/home/projects/vaccine/people/yatwan/tclustr/"
 # args : 1 = mainfolder ; 2 = outfolder ; 3 = n_epochs ; 4 = grep
 while getopts ":f:c:s:l:e:o:" opt; do
   case ${opt} in
@@ -32,10 +31,8 @@ while getopts ":f:c:s:l:e:o:" opt; do
     s )
       if [ "$OPTARG" == "htc" ]; then
         TBCRALIGN="$HTCPATH"
-        HOMEDIR="/home/projects2/riwa/tclustr/"
       elif [ "$OPTARG" == "c2" ]; then
         TBCRALIGN="$C2PATH"
-        HOMEDIR="/home/projects/vaccine/people/yatwan/tclustr/"
       fi
       ;;
     e )
@@ -62,7 +59,7 @@ done
 # Shift the processed options so that $1, $2, etc. now refer to non-option arguments
 shift $((OPTIND - 1))
 
-OUTDIR="${HOMEDIR}/output/${OUTPUTDIRECTORY}/"
+OUTDIR="$(realpath "$(pwd)/../output/${OUTPUTDIRECTORY}/")/"
 mkdir -pv $OUTDIR
 # Extract the basename without the extension using parameter expansion
 # Here assume we use the full TCRs
