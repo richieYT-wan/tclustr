b25c9bd7c0d1528a291fc9d32a4a82ada8a61fe9
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2025-03-07 00:03:32 +0100

    ready

---
+++ bashscripts/250306_IMMREP_vs_healthy.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+for seed in $(seq -f "%03g" 0 99);
+do
+
+  filename="IMMREP25_Ratio1_Run${seed}"
+script_content=$(cat <<EOF
+#! /usr/bin/bash
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate pynn
+HOMEDIR=/home/projects/vaccine/people/yatwan/tclustr/
+PYDIR=\${HOMEDIR}pyscripts/
+filename=${filename}
+cd \${PYDIR}
+python3 250306_cluster_against_healthy.py -seed ${seed} -o ${filename} -rid IMMREP25 -ratio 1 -n_jobs 4
+EOF
+)
+echo "$script_content" > "./250306_IMMREP25_v_garner/${filename}.sh"
+chmod +x "./250306_IMMREP25_v_garner/${filename}.sh"
+done
+
